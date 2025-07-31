a382cd57c02063ad1f7b9f84dce5ff1160cf2247
itaiglikman <itaig1998@gmail.com>
2025-07-31 18:02:09 +0300

    company model

---
+++ src/Models/Company.js
@@ -0,0 +1,13 @@
+class Company {
+    constructor(symbol, name, description, changes, price, currency, image) {
+        this.symbol = symbol;
+        this.name = name;
+        this.description = description;
+        this.changes = changes;
+        this.price = price;
+        this.currency = currency;
+        this.image = image;
+    }
+}
+
+export default Company;
